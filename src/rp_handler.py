d15f7c3e8bf8b7f8788879991f0dc0c53f45b0d1
asixia <130675778+asi-xia@users.noreply.github.com>
2025-04-14 23:34:14 +0800

    modified:   src/rp_handler.py

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -139,6 +139,7 @@ def handler(job):
         return {"state": "failed", 'message': 'task execution failed', "error": error_message}
     
     # run the inference
+    print(f"got task:{validated_data}")
     print(f"runpod-worker-VchitectXL - wait until video generation is complete")
     try:
         os.system(f'python3 -u inference.py --ckpt_path {MODEL_PATH} --propmt_text {validated_data["propmt"]} --cfg {validated_data["cfg"]} --steps {validated_data["steps"]} --duration {validated_data["duration"]} --resolution {validated_data["resolution"]}')
